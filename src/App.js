829ac3058a94c88f4e57fefcb2b75c13a6859c24
lif3ng <lif3ng@icloud.com>
2021-09-17 23:09:49 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,1 @@
+export default () => <div className="h-screen">app</div>;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./src/index.html", "./src/**/*.js"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
